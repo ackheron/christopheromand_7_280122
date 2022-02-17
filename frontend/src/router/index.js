f910bb9ca46cc0c5e741054e71108cc6241ef404
Ackheron <ackheron@etik.com>
2022-02-17 14:46:36 +0100

    Front, déclaration des routes messages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,21 @@
+/************************************** DECLARATIONS ET IMPORTATIONS  ******************************************/
+
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
+// Déclaration du composant Signup
 import Signup from "../components/Signup.vue";
 
+// Déclaration des composants Posts
+import AllPosts from "../components/posts/AllPosts.vue";
+import NewPost from "../components/posts/NewPost.vue";
+import OnePost from "../components/posts/OnePost.vue";
+import UpdatePost from "../components/posts/UpdatePost.vue";
+
 Vue.use(VueRouter);
+
+/************************************** ROUTERS  ******************************************/
 
 const routes = [
   {
@@ -16,6 +27,26 @@ const routes = [
     path: "/signup",
     name: "Signup",
     component: Signup,
+  },
+  {
+    path: "/posts",
+    name: "AllPosts",
+    component: AllPosts,
+  },
+  {
+    path: "/posts/create",
+    name: "NewPost",
+    component: NewPost,
+  },
+  {
+    path: "/posts/:id",
+    name: "OnePost",
+    component: OnePost,
+  },
+  {
+    path: "/posts/update/:id",
+    name: "UpdatePost",
+    component: UpdatePost,
   },
 ];
 
